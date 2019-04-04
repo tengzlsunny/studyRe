99b93c756f22a5338932877e26ef531271d5279a
tengzlsunny <tengzlsunny@163.com>
2019-04-04 16:38:35 +0800

    react

--- README.md
+++ README.md
@@ -373,6 +373,133 @@
                 })
             ```
 
+        13. react创建组件的三种方式
+            1. 无状态函数式组件
+                > 组件不会被实例化，整体渲染性能得到提升
+                > 组件不能访问this对象
+                > 组件无法访问生命周期的方法
+                > 无状态组件只能访问输入的props，同样的props会得到同样的渲染结果，不会有副作用
+                ```js
+                    const TypePublic = (props) => {
+                        return (
+                            <div className="type_p">
+                                <p className="type_title">{props.title}</p>
+                            </div>
+                        )
+                    }
+                ```
+            2. React.createClass(有状态函数)
+                > React.createClass会自绑定函数方法
+                > React.createClass的mixins不够自然、直观
+
+            3. React.Component(有状态函数)
+                > 可以更好实现代码复用
+
+            * React.createClass与React.Component区别
+                > this指向
+                React.createClass创建的组件，其每一个成员函数的this都有React自动绑定，任何时候使用,函数中的this会被正确设置。
+                React.Component创建的组件，其成员函数不会自动绑定this，需要开发者手动绑定，否则this不能获取当前组件实例对象。
+                React.Component有三种手动绑定方法：
+                * 可以在构造函数中完成绑定，也可以在调用时使用method.bind(this)来完成绑定，还可以使用arrow function来绑定。
+                ```js
+                 constructor(props) {
+                    super(props);
+                    this.handleClick = this.handleClick.bind(this); //构造函数中绑定
+                }
+                <div onClick={this.handleClick.bind(this)}></div> //使用bind来绑定
+                <div onClick={()=>this.handleClick()}></div> //使用arrow function来绑定
+                ```
+
+                > 组件属性类型propTypes及其默认props属性defaultProps配置不同
+                React.createClass在创建组件时，有关组件props的属性类型及组件默认的属性会作为组件实例的属性来配置，其中defaultProps是使用getDefaultProps的方法来获取默认组件属性的
+                ```js
+                const TodoItem = React.createClass({
+                    propTypes: { // as an object
+                        name: React.PropTypes.string
+                    },
+                    getDefaultProps(){   // return a object
+                        return {
+                            name: ''    
+                        }
+                    }
+                    render(){
+                        return <div></div>
+                    }
+                })
+                ```
+                React.Component在创建组件时配置这两个对应信息时，他们是作为组件类的属性，不是组件实例的属性，也就是所谓的类的静态属性来配置的。对应上面配置如下
+                ```js
+                class TodoItem extends React.Component {
+                    static propTypes = {//类的静态属性
+                        name: React.PropTypes.string
+                    };
+
+                    static defaultProps = {//类的静态属性
+                        name: ''
+                    };
+
+                    ...
+                }
+                ```
+
+                > 组件初始状态state的配置不同
+                React.createClass创建的组件，其状态state是通过getInitialState方法来配置组件相关的状态；
+                ```js
+                const TodoItem = React.createClass({
+                    // return an object
+                    getInitialState(){ 
+                        return {
+                            isEditing: false
+                        }
+                    }
+                    render(){
+                        return <div></div>
+                    }
+                })
+                ```
+                React.Component创建的组件，其状态state是在constructor中像初始化组件属性一样声明的。
+                ```js
+                class TodoItem extends React.Component{
+                    constructor(props){
+                        super(props);
+                        this.state = { // define this.state in constructor
+                            isEditing: false
+                        } 
+                    }
+                    render(){
+                        return <div></div>
+                    }
+                }
+                ```
+
+                > Mixins的支持不同
+                Mixins(混入)是面向对象编程OOP的一种实现，其作用是为了复用共有的代码，将共有的代码通过抽取为一个对象，然后通过Mixins进该对象来达到代码复用。具体可以参考React Mixin的前世今生。
+
+                React.createClass在创建组件时可以使用mixins属性，以数组的形式来混合类的集合。
+                ```js
+                var SomeMixin = {  
+                    doSomething() {
+
+                    }
+                };
+                const Contacts = React.createClass({  
+                    mixins: [SomeMixin],
+                    handleClick() {
+                        this.doSomething(); // use mixin
+                    },
+                    render() {
+                        return (
+                        <div onClick={this.handleClick}></div>
+                        );
+                    }
+                });
+                ```
+                React.Component这种形式并不支持Mixins，至今React团队还没有给出一个该形式下的官方解决方案；但是React开发者社区提供一个全新的方式来取代Mixins,那就是Higher-Order Components，
+
+            * 创建组件的形式选择还应该根据下面来决定：
+            1. 只要有可能，尽量使用无状态组件创建形式。
+            2. 否则（如需要state、生命周期方法等），使用`React.Component`这种es6形式创建组件
+
 
              
 
--- app/src/pages/home/home.js
+++ app/src/pages/home/home.js
@@ -16,69 +16,48 @@ import banner5 from '@/assets/imgs/5.jpg'
 import banner6 from '@/assets/imgs/6.jpg'
 import banner7 from '@/assets/imgs/7.jpg'
 import banner8 from '@/assets/imgs/8.jpg'
+import { create } from 'domain';
 
 
 export default class NavigationBar extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            recommend: [],
             headerSelect: 0
 
         }
     }
-    componentDidMount() {
-        let that = this
-        // api.axios.get(api.apiUrl + 'banner')
-        // .then(res => {
-        //     this.setState({
-        //         banner: res.data.banners
-        //     })
-        // })
-        api.axios.get('./app/static/json/tj.json')
-        .then(res => {
-            that.setState({
-                recommend: res.data.data
-            })
+
+    titleSelect(i) {
+        this.setState({
+            headerSelect: i
         })
     }
 
+
     render() {
         let that = this
-       
-
-        let recommend = that.state.recommend
         let headerSelect = that.state.headerSelect
-
-        if (recommend.length) {
-            var tjList = recommend.map((v, i) => {
-                return (
-                    <li key={i}>
-                        <Link to={v.route}>
-                            <span className={v.icon +' iconfont'}></span>
-                            <p>{v.content}</p>
-                        </Link>
-                    </li>
-                )
-            })
+        let pageChange = null
+        
+        if (headerSelect == 0) {
+            pageChange = <Home0 />
+        } else if (headerSelect == 1) {
+            pageChange = <Home1 />
+        } else if (headerSelect == 2) {
+            pageChange = <Home2 />
         }
         return (
             <div className="home_index">
                 <Header/>
                 
                 <ul className="home_header">
-                    <li className={ headerSelect == 0 ? 'header_select' : ''}><p>推荐</p></li>
-                    <li className={ headerSelect == 1 ? 'header_select' : ''}><p>朋友</p></li>
-                    <li className={ headerSelect == 2 ? 'header_select' : ''}><p>电台</p></li>
+                    <li className={ headerSelect == 0 ? 'header_select' : ''} onClick={this.titleSelect.bind(this,0)}><p>推荐</p></li>
+                    <li className={ headerSelect == 1 ? 'header_select' : ''} onClick={this.titleSelect.bind(this,1)}><p>朋友</p></li>
+                    <li className={ headerSelect == 2 ? 'header_select' : ''} onClick={this.titleSelect.bind(this,2)}><p>电台</p></li>
                 </ul>
 
-                <Home0 />
-
-                <ul className="tj">
-                    {tjList}
-                </ul>
-
-                <Typelist type="1"/>
+                {pageChange}
 
                 <Footer text="0" />
 
@@ -125,7 +104,8 @@ class Home0 extends React.Component{
                     imgUrl: banner8,
                     addUrl: 'https://www.baidu.com'
                 }
-            ]
+            ],
+            recommend: [],
         }
     }
 
@@ -137,9 +117,17 @@ class Home0 extends React.Component{
         //         banner: res.data.banners
         //     })
         // })
+        api.axios.get('./app/static/json/tj.json')
+        .then(res => {
+            that.setState({
+                recommend: res.data.data
+            })
+        })
     }
 
     render() {
+        let that = this
+        let recommend = that.state.recommend
         let options = {
             showPot: true,  //是否显示下方pot 默认为true
             timeGap: 3000,   //时间间隔 默认3000
@@ -147,30 +135,101 @@ class Home0 extends React.Component{
             showClick: false, // 是否显示左右箭头
             banner: this.state.banner
         };
+
+        if (recommend.length) {
+            var tjList = recommend.map((v, i) => {
+                return (
+                    <li key={i}>
+                        <Link to={v.route}>
+                            <span className={v.icon +' iconfont'}></span>
+                            <p>{v.content}</p>
+                        </Link>
+                    </li>
+                )
+            })
+        }
+
         return (
             <div className="home0">
                 <div className="home0_banner">
                     <div className="home0_bk"></div>
                     <Slider options={options} />
                 </div>
+                <ul className="tj">
+                    {tjList}
+                </ul>
+                <TypePublic title={1}/>
             </div>
         )
     }
 }
+// 类型title 无状态组件())
+/**
+ * 组件不会被实例化，整体渲染性能得到提升
+ * 组件不能访问this对象
+ * 组件无法访问生命周期的方法
+ * 无状态组件只能访问输入的props，同样的props会得到同样的渲染结果，不会有副作用
+ */
+const TypePublic = (props) => {
+    return (
+        <div className="type_p">
+            <p className="type_title">{props.title}<i className="iconfont">&#xe617;</i></p>
+            <ul className="type_list">
+                <li>
+                    <img src={banner1} alt=""/>
+                    <p>1111111</p>
+                </li>
+                <li>
+                    <img src={banner1} alt=""/>
+                    <p>22222222222222</p>
+                </li>
+                <li>
+                    <img src={banner1} alt=""/>
+                    <p>333333333333333</p>
+                </li>
+            </ul>
+            
+        </div>
+    )
+}
 
-// 类型列表
-class Typelist extends React.Component{
+
+// 朋友
+class Home1 extends React.Component{
     constructor(props) {
-        super(props)
+        super(props);
+        this.state = {
+            
+        }
     }
 
     render() {
         return (
             <div>
-                <div>
-                   11111
-                </div>
+                <p>home1</p>
             </div>
         )
     }
 }
+
+
+// 电台
+class Home2 extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <p>home2</p>
+            </div>
+        )
+    }
+}
+
+
+
